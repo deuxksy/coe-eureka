f21710e9e0dc38a9c8f7213c38de6a1917592b5f
Marrakesh ACT <actmember@bas1-quebec13-70-30-181-71.dsl.bell.ca>
2018-06-11 15:10:12 +0900

    add logback.xml

--- src/main/java/com/sds/ces/eureka/CoeEurekaApplication.java
+++ src/main/java/com/sds/ces/eureka/CoeEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.example.coeeureka;
+package com.sds.ces.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sds/ces/eureka/CoeEurekaApplicationTests.java
+++ src/test/java/com/sds/ces/eureka/CoeEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.coeeureka;
+package com.sds.ces.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
