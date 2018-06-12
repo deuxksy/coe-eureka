e1a846535c9d371ddc6ea19b810436afe5ddd926
Marrakesh ACT <actmember@bas1-quebec13-70-30-181-71.dsl.bell.ca>
2018-06-12 11:08:16 +0900

    start know

--- src/main/java/com/sds/ces/eureka/CoeEurekaApplication.java
+++ src/main/java/com/sds/ces/eureka/CoeEurekaApplication.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class CoeEurekaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CoeEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoeEurekaApplication.class, args);
+    }
 }
